60e0c138df6174126925af7f56566dbda643903a
Sujit Kumar <raos92249@gmail.com>
2022-10-01 23:01:57 +0530

    I am commiting Sum.java that is right now modified so technically older version of it will be added where only number is being printed

---
+++ Sum.java
@@ -0,0 +1,14 @@
+class Sum{
+
+    public static void main(String[] args) {
+        int Sum;
+        int a = 6;
+        int b = 9;
+        Sum= a + b;
+        System.out.println(Sum);
+    }
+
+
+
+
+}
