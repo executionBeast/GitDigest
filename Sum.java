1e684a12638cc922fb551c1e3120970b57a07623
Sujit Kumar <raos92249@gmail.com>
2022-10-01 23:26:47 +0530

    Commiting the modified Sum.java where Difference of 6 and 9 is also shown

--- Sum.java
+++ Sum.java
@@ -1,14 +1,13 @@
 class Sum{
 
     public static void main(String[] args) {
-        int Sum;
         int a = 6;
         int b = 9;
-        Sum= a + b;
+        int Sum= a + b;
+        int Diff = a - b;
+        System.out.println("The difference of 6 and 9 is : "+ Diff);
         System.out.println("The sum of 6 and 9 is : "+ Sum );
+        System.out.println("hey this java file is for learning Git");
     }
-
-
-
 
 }
