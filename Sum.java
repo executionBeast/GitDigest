f501e93bbe95ba7ca0f7897c414426476b4264f5
Sujit Kumar <raos92249@gmail.com>
2022-10-01 23:30:34 +0530

    Commiting Sum.java having Syntax Error

--- Sum.java
+++ Sum.java
@@ -5,8 +5,8 @@ class Sum{
         int b = 9;
         int Sum= a + b;
         int Diff = a - b;
-        System.out.println("The difference of 6 and 9 is : "+ Diff);
-        System.out.println("The sum of 6 and 9 is : "+ Sum );
+        System.jhfout.println("The difference of 6 and 9 is : "+ Diff);
+        System.khout.println("The sum of 6 and 9 is : "+ Sum );
         System.out.println("hey this java file is for learning Git");
     }
 
